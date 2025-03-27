bc1329924f68353606b30038f41bb4b5d606c087
Charles <shoubing@gmail.com>
2025-03-27 15:37:03 -0700

    added --load_in_4bit and --lora_rank

--- examples/scripts/train_rm_llama_no_flash_attn.sh
+++ examples/scripts/train_rm_llama_no_flash_attn.sh
@@ -22,7 +22,7 @@ openrlhf.cli.train_rm \
    --load_checkpoint \
    --gradient_checkpointing \
    --load_in_4bit \
-   --lora_rank 16 \
+   --lora_rank 16
 EOF
      # --use_wandb [WANDB_TOKENS] or True (use wandb login command)
      # --packing_samples
