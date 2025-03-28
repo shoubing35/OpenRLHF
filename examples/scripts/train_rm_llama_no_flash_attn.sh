10b1c8701907dbfd8693ecf929bc933e279433c5
shoubing35 <shoubing@gmail.com>
2025-03-28 08:26:23 -0700

    add back 4bit

--- examples/scripts/train_rm_llama_no_flash_attn.sh
+++ examples/scripts/train_rm_llama_no_flash_attn.sh
@@ -20,7 +20,9 @@ openrlhf.cli.train_rm \
    --rejected_key rejected \
    --load_checkpoint \
    --gradient_checkpointing \
-   --bf16
+   --bf16 \
+   --load_in_4bit \
+   --lora_rank 32
 EOF
      # --use_wandb [WANDB_TOKENS] or True (use wandb login command)
      # --packing_samples
