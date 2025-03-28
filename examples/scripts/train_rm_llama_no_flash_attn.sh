eb96738c157f4f0f13766f4c659ddb78bfc3c50c
charles <charleschen@charless-imac.lan>
2025-03-28 07:55:11 -0700

    change max_len to 512 from 2048

--- examples/scripts/train_rm_llama_no_flash_attn.sh
+++ examples/scripts/train_rm_llama_no_flash_attn.sh
@@ -10,7 +10,7 @@ openrlhf.cli.train_rm \
    --micro_train_batch_size 1 \
    --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
    --max_epochs 1 \
-   --max_len 2048 \
+   --max_len 512 \
    --max_samples 10 \
    --zero_stage 2 \
    --learning_rate 9e-6 \
