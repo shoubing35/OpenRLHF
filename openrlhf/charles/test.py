7e9cbb2b0964d0d4848ebbb221920e96878f0419
shoubing35 <shoubing@gmail.com>
2025-04-01 09:58:07 -0700

    add train() function (no calls yet)

--- openrlhf/charles/test.py
+++ openrlhf/charles/test.py
@@ -1,1 +1,42 @@
 print("hi charles")
+
+import argparse
+import math
+import os
+from datetime import datetime
+
+from transformers.trainer import get_scheduler
+
+from openrlhf.datasets import RewardDataset
+from openrlhf.models import get_llm_for_sequence_regression
+from openrlhf.trainer import RewardModelTrainer
+from openrlhf.utils import blending_datasets, get_strategy, get_tokenizer
+
+
+def train(args):
+    # configure strategy
+    strategy = get_strategy(args)
+    strategy.setup_distributed()
+
+    # configure model
+    # load huggingface model/config
+    model = get_llm_for_sequence_regression(
+        args.pretrain,
+        # "reward", # charles comment out
+        model_type="reward", # charles
+        use_flash_attention_2=args.flash_attn,
+        bf16=args.bf16,
+        load_in_4bit=args.load_in_4bit,
+        lora_rank=args.lora_rank,
+        lora_alpha=args.lora_alpha,
+        target_modules=args.target_modules,
+        lora_dropout=args.lora_dropout,
+        ds_config=strategy.get_ds_train_config(is_actor=False),
+        init_value_head=True,
+        value_head_prefix=args.value_head_prefix,
+        packing_samples=args.packing_samples,
+        # is_rm=True, # charles
+    )
+
+    # configure tokenizer
+    tokenizer = get_tokenizer(args.pretrain, model, "left", strategy, use_fast=not args.disable_fast_tokenizer)
