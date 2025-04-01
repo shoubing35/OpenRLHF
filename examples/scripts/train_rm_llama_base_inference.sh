ddabf797203d9a8b32897868d9112b5f523c518a
Charles <shoubing@gmail.com>
2025-04-01 12:51:21 -0700

    created base_inference.sh from no_flash_attn.sh, changed --lora_rank to 0 to NOT create any trainable lora layers

---
+++ examples/scripts/train_rm_llama_base_inference.sh
@@ -0,0 +1,33 @@
+set -x
+
+read -r -d '' training_commands <<EOF
+openrlhf.cli.train_rm \
+   --save_path ./checkpoint/llama3-8b-rm \
+   --save_steps -1 \
+   --logging_steps 1 \
+   --eval_steps -1 \
+   --train_batch_size 1 \
+   --micro_train_batch_size 1 \
+   --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
+   --max_epochs 1 \
+   --max_len 512 \
+   --max_samples 10 \
+   --zero_stage 2 \
+   --learning_rate 9e-6 \
+   --dataset OpenRLHF/preference_dataset_mixture2_and_safe_pku \
+   --apply_chat_template \
+   --chosen_key chosen \
+   --rejected_key rejected \
+   --load_checkpoint \
+   --gradient_checkpointing \
+   --bf16 \
+   --load_in_4bit \
+   --lora_rank 0
+EOF
+     # --use_wandb [WANDB_TOKENS] or True (use wandb login command)
+     # --packing_samples
+
+
+if [[ ${1} != "slurm" ]]; then
+    deepspeed --module $training_commands
+fi
