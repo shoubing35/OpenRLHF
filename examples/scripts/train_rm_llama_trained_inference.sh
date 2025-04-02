97105eb9bca2f3a9f9b0f65933be71e2f17d4fa2
shoubing35 <shoubing@gmail.com>
2025-04-01 20:31:37 -0700

    load llama-3-8b-rm-combined in trained_inference.sh

--- examples/scripts/train_rm_llama_trained_inference.sh
+++ examples/scripts/train_rm_llama_trained_inference.sh
@@ -7,7 +7,7 @@ openrlhf.charles.test \
    --eval_steps -1 \
    --train_batch_size 1 \
    --micro_train_batch_size 1 \
-   --pretrain ./checkpoint/llama3-8b-rm \
+   --pretrain ./checkpoint/llama-3-8b-rm-combined \
    --max_epochs 1 \
    --max_len 512 \
    --max_samples 10 \
